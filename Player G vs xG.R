0146e883be0b1723484c8c3aafc2bb42c6f3f7fa
Conor Tompkins <conorotompkins@gmail.com>
2016-11-27 14:36:23 -0500

    worked on actual vs expected R file

--- Player G vs xG.R
+++ Player G vs xG.R
@@ -4,30 +4,100 @@ library(ggplot2)
 library(lubridate)
 library(tidyr)
 
-data <- read_csv("Corsica_Skater.Stats_11h47.csv") %>%
-        filter(Team == "PIT") %>%
-        arrange(Date) %>%
-        mutate(iG60 = (G * TOI) / 60,
-               game_number = dense_rank(Date)) %>%
-        select(Player, Date, game_number, ixG60, iG60) %>%
-        gather(., metric, measure, -Player, -Date, -game_number)
+crosby <- read_csv("g vs xg.csv") %>%
+        filter(Player == "SIDNEY.CROSBY") %>%
+        select(Player, Date, TOI, ixG60, G60) %>%
+        arrange(Player, Date) %>%
+        mutate(game_number = dense_rank(Date), 
+               Actual_Minus_Expected = G60 - ixG60)
 
-data$metric[data$metric == "iG60"] <- "Actual Goals per 60"
-data$metric[data$metric == "ixG60"] <- "Expected Goals per 60"
+crosby <- crosby %>%
+        gather(., metric, measure, -Player, -Date, -game_number, -TOI)
 
-ggplot(data, aes(game_number, measure, color = metric, fill = metric)) +
-        geom_point(alpha = .1) +
-        geom_smooth(span = .3) +
+crosby$metric[crosby$metric == "G60"] <- "Actual Goals"
+crosby$metric[crosby$metric == "ixG60"] <- "Expected Goals"
+
+crosby %>%
+        filter(Player == "SIDNEY.CROSBY" & metric == "Actual Goals") %>%
+        View()
+
+crosby_graph1 <- crosby %>%
+        filter(metric != "Actual_Minus_Expected") %>%
+        ggplot(., aes(game_number, measure, color = metric, fill = metric)) +
+        geom_point(size = .5, alpha = I(.2)) +
+        geom_smooth(span = .2) +
         facet_wrap(~metric) +
-        coord_cartesian(ylim = c(0, 2)) +
+        coord_cartesian(ylim = c(0, 7)) +
         theme_bw() +
         labs(x = "Game Number",
-             y = "Metric",
+             y = "Per 60",
              title = "Sidney Crosby 2007-2016",
              subtitle = "5v5",
              caption = "@Null_HHockey, data from http://www.corsica.hockey") +
-        guide_legend(direction = "vertical") +
-        theme(legend.position = "bottom")
+        theme(legend.position = "bottom",
+              legend.title = element_blank())
+ggsave("Crosby Actual vs Expected.png")
+print(crosby_graph1)
+
+crosby_graph2 <- crosby %>%
+        filter(metric == "Actual_Minus_Expected") %>%
+        ggplot(., aes(game_number, measure)) +
+        geom_hline(yintercept = 0) +
+        geom_point(size = .5, alpha = I(.2)) +
+        geom_smooth(span = .2) +
+        facet_wrap(~metric) +
+        coord_cartesian(ylim = c(-5, 5)) +
+        theme_bw() +
+        labs(x = "Game Number",
+             y = "Per 60",
+             title = "Sidney Crosby 2007-2016",
+             subtitle = "5v5",
+             caption = "@Null_HHockey, data from http://www.corsica.hockey")
+print(crosby_graph2)
 
 
+ovechkin <- read_csv("g vs xg.csv") %>%
+        filter(Player == "ALEX.OVECHKIN") %>%
+        select(Player, Date, TOI, ixG60, G60) %>%
+        arrange(Player, Date) %>%
+        mutate(game_number = dense_rank(Date), 
+               Actual_Minus_Expected = G60 - ixG60)
+
+ovechkin <- ovechkin %>%
+        gather(., metric, measure, -Player, -Date, -game_number, -TOI)
+
+ovechkin$metric[ovechkin$metric == "G60"] <- "Actual Goals"
+ovechkin$metric[ovechkin$metric == "ixG60"] <- "Expected Goals"
+ovechkin_graph1 <- ovechkin %>%
+        filter(metric != "Actual_Minus_Expected") %>%
+        ggplot(., aes(game_number, measure, color = metric, fill = metric)) +
+        geom_point(size = .5, alpha = I(.2)) +
+        geom_smooth(span = .2) +
+        facet_wrap(~metric) +
+        coord_cartesian(ylim = c(0, 7)) +
+        theme_bw() +
+        labs(x = "Game Number",
+             y = "Per 60",
+             title = "Alexander Ovechkin 2007-2016",
+             subtitle = "5v5",
+             caption = "@Null_HHockey, data from http://www.corsica.hockey") +
+        theme(legend.position = "bottom",
+              legend.title = element_blank())
+print(ovechkin_graph1)
+
+ovechkin_graph2 <- ovechkin %>%
+        filter(metric == "Actual_Minus_Expected") %>%
+        ggplot(., aes(game_number, measure)) +
+        geom_hline(yintercept = 0) +
+        geom_point(size = .5, alpha = I(.2)) +
+        geom_smooth(span = .2) +
+        facet_wrap(~metric) +
+        coord_cartesian(ylim = c(-5, 5)) +
+        theme_bw() +
+        labs(x = "Game Number",
+             y = "Per 60",
+             title = "Alex Ovechkin 2007-2016",
+             subtitle = "5v5",
+             caption = "@Null_HHockey, data from http://www.corsica.hockey")
+print(ovechkin_graph2)
 
